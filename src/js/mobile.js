6fcf2c46f15f42e7d52392ed32d2c8d992e50b82
A. Santos <angelo.rws@gmail.com>
2025-11-01 15:16:09 -0700

    Fixed structure

--- src/js/mobile.js
+++
@@ -1,7 +0,0 @@
-var container = document.getElementById('mobile-modal-container');
-var ignoreButton = document.getElementById('ignore-mobile');
-
-ignoreButton.addEventListener('click', function(e) {
-    container.style = 'display: none;';
-    console.log('click');
-});
--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-console.log(`
-        へ  ♡ ╱|、
-     ૮ - ՛)   (\` - 7
-     / ⁻ ៸|    |、⁻〵
- 乀 (ˍ,ل ل    じしˍ,)ノ
-
-theres nothing to see here :3`);
