66a706ca761758b59b53705c12fe0c4b70d65692
A. Santos <angelo.rws@gmail.com>
2026-03-03 03:07:09 -0800

    actually fix 404 page, for real this time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
 
 import react from '@astrojs/react';
 
@@ -18,5 +20,21 @@ export default defineConfig({
       "/res/images/hotlink-ok/button.gif": "/hotlink-ok/button.gif"
   },
 
-  integrations: [react()],
+  integrations: [react(), 
+    {
+    name: 'rename-not-found',
+    hooks: {
+      'astro:build:done': ({ dir }) => {
+        const distDir = fileURLToPath(dir);
+        const oldFile = `${distDir}404.html`;
+        const newFile = `${distDir}not_found.html`;
+
+        if (fs.existsSync(oldFile)) {
+          fs.renameSync(oldFile, newFile);
+          console.log('moved not_found.html to root');
+        }
+      }
+    }
+  }
+  ],
 });
